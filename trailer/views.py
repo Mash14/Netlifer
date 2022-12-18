51c30cc0cf1ef39e35e71c95f144504b95e461fe
Mash14 <mashalonzo741@gmail.com>
2022-12-18 10:27:10 +0300

    Search for movies from moviedb

--- trailer/views.py
+++ trailer/views.py
@@ -127,3 +127,84 @@ def single_movie(request,movie_id):
     
     return render(request, 'movie.html', {'movie':movie,'similar_movies':similar_movies,'url':url,'title':title})
 
+
+def single_tv_show(request,tv_show_id):
+    tv_show_url = f'https://api.themoviedb.org/3/tv/{tv_show_id}?api_key={api_key}&language=en-US'
+    response = requests.get(tv_show_url)
+    tv_show = response.json()
+    # movie = data['results']
+
+    # Similar movies
+    similar_tv_shows_url = f'https://api.themoviedb.org/3/tv/{tv_show_id}/similar?api_key={api_key}&language=en-US&page=1'
+    response = requests.get(similar_tv_shows_url)
+    data = response.json()
+    similar_tv_shows = data['results']
+
+    #Youtube videos
+    youtube_url = 'https://www.googleapis.com/youtube/v3/search'
+    params = {
+        'part' : 'snippet',
+        'key' : youtube_api_key,
+        'maxRsults' : 1,
+        'q' : tv_show['original_name'] + 'official trailer',
+    }
+
+    response = requests.get(youtube_url,params=params)
+    data = response.json()
+    id = data['items'][0]['id']['videoId']
+    url = f'https://www.youtube.com/embed/{id}'
+
+    title = f"{tv_show['original_name']}"
+    
+    return render(request, 'tv_show.html', {'tv_show':tv_show,'similar_tv_shows':similar_tv_shows,'url':url,'title':title})
+
+
+def search(request):
+    search_term = request.GET.get('search_term')
+    movies = []
+    
+    # Page 1
+    search_url = f'https://api.themoviedb.org/3/search/multi?api_key={api_key}&language=en-US&query={search_term}&page=1&include_adult=false'
+    response = requests.get(search_url)
+    data = response.json()
+    searched_movies = data['results']
+        
+    for m in searched_movies:
+        if m['media_type'] == 'tv' or m['media_type'] == 'movie':
+            movies.append(m)
+
+    # Page 2
+    search_url = f'https://api.themoviedb.org/3/search/multi?api_key={api_key}&language=en-US&query={search_term}&page=2&include_adult=false'
+    response = requests.get(search_url)
+    data = response.json()
+    searched_movies = data['results']
+        
+    for m in searched_movies:
+        if m['media_type'] == 'tv' or m['media_type'] == 'movie':
+            movies.append(m)
+
+    # Page 3
+    search_url = f'https://api.themoviedb.org/3/search/multi?api_key={api_key}&language=en-US&query={search_term}&page=3&include_adult=false'
+    response = requests.get(search_url)
+    data = response.json()
+    searched_movies = data['results']
+        
+    for m in searched_movies:
+        if m['media_type'] == 'tv' or m['media_type'] == 'movie':
+            movies.append(m)
+
+    # Page 4
+    search_url = f'https://api.themoviedb.org/3/search/multi?api_key={api_key}&language=en-US&query={search_term}&page=4&include_adult=false'
+    response = requests.get(search_url)
+    data = response.json()
+    searched_movies = data['results']
+        
+    for m in searched_movies:
+        if m['media_type'] == 'tv' or m['media_type'] == 'movie':
+            movies.append(m)
+
+    title = f"Search '{search_term}'"
+    
+
+    return render(request,'search.html',{'movies':movies,'search_term':search_term,'title':title})
+
